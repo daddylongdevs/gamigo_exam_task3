5e3f511c5b8fcda734002925361e2da88e718e74
daddylongdevs <jlbtirona@gmail.com>
2026-05-30 13:49:22 +0800

    feat: added SendMonsterDataRequest and MonsterDataReceived functions to ClientPacketsHandler

--- Assets/Test Tasks/Editable/ClientPacketsHandler.cs
+++ Assets/Test Tasks/Editable/ClientPacketsHandler.cs
@@ -15,12 +15,32 @@ namespace TestTask.Editable
 
             ClientManager.Instance.SetClientLogInStatus(responseCode, clientId);
         }
+
+        public static void MonsterDataReceived(Packet packet)
+        {
+            Debug.Log("ClientPacketsHandler: MonsterDataReceived");
+
+            int monsterId = packet.ReadInt();
+            int monsterType = packet.ReadInt();
+            float monsterMaxHealth = packet.ReadFloat();
+            float monsterCurrentHealth = packet.ReadFloat();
+            
+            Debug.Log($"ClientPacketsHandler: MonsterDataReceived: Id: {monsterId}, Type: {monsterType}, Max Health: {monsterMaxHealth}, Current Health: {monsterCurrentHealth}");
+        }
         #endregion
 
         #region Packet Senders
         public static void SendLoginRequest()
         {
             Packet packet = new Packet(1);
+            ClientManager.Instance.PacketSenderClient.SendToServer(packet);
+        }
+
+        public static void SendMonsterDataRequest()
+        {
+            Debug.Log("ClientPacketsHandler: SendMonsterDataRequest");
+
+            Packet packet = new Packet(2);
             ClientManager.Instance.PacketSenderClient.SendToServer(packet);
         }
         #endregion

5066c993255aaec6a0271383c8b625dcd0747b4d
daddylongdevs <jlbtirona@gmail.com>
2026-06-01 13:51:24 +0800

    feat: added monster UI handling via ClientMobsUIManager fix: removed GetMonsterData method in login onPress event fix: call GetMonsterData on successful login

---
+++ Assets/Test Tasks/Editable/UIManagers/ClientMobsUIManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TestTask.NonEditable;
+using TestTask.Editable;
+using UnityEngine.UI;
+using TMPro;
+
+namespace TestTask.Editable
+{
+    public class ClientMobsUIManager : MonoBehaviour
+    {
+        [field: SerializeField] private Sprite DefaultMonsterIcon;
+        [field: SerializeField] private Sprite[] MonsterIcons;
+        [field: SerializeField] private Image MonsterIcon;
+        [field: SerializeField] private Image MonsterHealthBar;
+        [field: SerializeField] private TMP_Text MonsterName;
+
+        void OnEnable()
+        {
+            ClientMobsManager.OnMonsterDataReceived += OnNewMonsterSpawned;
+        }
+
+        void OnDisable()
+        {
+            ClientMobsManager.OnMonsterDataReceived -= OnNewMonsterSpawned;
+        }
+
+        public void OnNewMonsterSpawned(MonsterData monsterData)
+        {
+            Debug.Log("ClientMobsUIManager: OnNewMonsterSpawned: " + monsterData.MonsterName);
+
+            UpdateMonsterName(monsterData.MonsterName);
+            UpdateMonsterIcon(monsterData.MonsterName);
+            UpdateMonsterHealthBar(monsterData.MonsterCurrentHealth / monsterData.MonsterMaxHealth);
+        }
+
+        private void UpdateMonsterName(string monsterName)
+        {
+            MonsterName.text = monsterName;
+        }
+
+        private void UpdateMonsterIcon(string monsterName)
+        {
+            MonsterIcon.sprite = GetMonsterIconSprite(monsterName);
+        }
+
+        private void UpdateMonsterHealthBar(float healthPercentage)
+        {
+            float fillAmount = Mathf.Clamp01(healthPercentage);
+            MonsterHealthBar.fillAmount = fillAmount;
+        }
+
+        private Sprite GetMonsterIconSprite(string monsterName)
+        {
+            foreach (var icon in MonsterIcons)
+            {
+                if (icon.name == monsterName)
+                {
+                    return icon;
+                }
+            }
+            return DefaultMonsterIcon;
+        }
+    }
+}

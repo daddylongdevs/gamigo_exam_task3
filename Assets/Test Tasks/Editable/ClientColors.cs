c738c885600e05c3c20c7d26ba43f51568cd641c
daddylongdevs <jlbtirona@gmail.com>
2026-06-02 00:17:46 +0800

    feat: request color set client -> server -> client on button press feat: color display ui on successful server response

--- Assets/Test Tasks/Editable/ClientColors.cs
+++ Assets/Test Tasks/Editable/ClientColors.cs
@@ -1,10 +1,23 @@
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 namespace TestTask.Editable
 {
     public class ClientColors : MonoBehaviour
     {
+        public static event Action<Color[]> OnColorSetReceived;
+        
+        public Color[] Colors { get; private set; }
 
+        public void GetColorSet()
+        {
+            ClientPacketsHandler.SendColorSetRequest();
+        }
+
+        public void NotifyColorSetReceived(Color[] colors)
+        {
+            Colors = colors;
+            OnColorSetReceived?.Invoke(Colors);
+        }
     }
 }
